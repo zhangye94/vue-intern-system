14ad3f87829f2120c6604371db9397b156f47bf0
Zhang Ye <727935886@qq.com>
2017-06-06 14:59:04 +0800

    1.095 评价指标页添加标签页

--- src/page/assessment/indexSetting.vue
+++ src/page/assessment/indexSetting.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    internshipProcess
+  <div id="index-setting-model">
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="校内教师评价指标" name="first">
+
+        </el-tab-pane>
+        <el-tab-pane label="校外教师评价指标" name="second">
+
+        </el-tab-pane>
+        <el-tab-pane label="学生评价指标" name="third">
+
+        </el-tab-pane>
+      </el-tabs>
   </div>
 </template>
 
@@ -9,10 +19,12 @@
     components: {
     },
     created: function () {
+      this.$store.state.adminleftnavnum="/assessment/indexSetting";
     },
     data () {
       return {
-
+        root: localStorage.root,
+        activeName: 'first'
       }
     },
     methods: {
